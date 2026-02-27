44c3f92e045a5522a3ec6a51236c9c731bde1784
junyoung-hong <junyoung.hong@stu.ocsb.ca>
2026-02-27 12:39:48 -0500

    TEMPERATURE CODE

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Junyoung
+ * Created on: Feb 2026
+ * This program shows the temperature with K
 */
 
-basic.showString('Hello, World!')
+// our variable for a random number
+let temperatureValueCelsius: number
+let temperatureValueKelvin: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // get temperature value
+    temperatureValueCelsius = input.temperature()
+    temperatureValueKelvin = temperatureValueCelsius + 273.15
+    // output answer
+    basic.clearScreen()
+    basic.showString('The temperature is:' + temperatureValueKelvin.toString() + 'K')
+})
